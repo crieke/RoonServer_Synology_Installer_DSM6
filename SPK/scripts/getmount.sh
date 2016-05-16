1cba44c50496f6b77d3d8aad3dbee013a7daa12a
Christopher Rieke <github@mail.rieke.tv>
2016-05-16 17:23:35 +0200

    More precise on checking Volumes

--- SPK/scripts/getmount.sh
+++ SPK/scripts/getmount.sh
@@ -2,8 +2,8 @@
 #Checking for RoonServer share on internal volumes
 ROON_SHARE="RoonServer"
 
-VOLS=`ls -d /volume* | sed -n '/USB/!p'`
-for i in $VOLS; do
+VOLUMES=`ls -d /volume[0-9]`
+for i in $VOLUMES; do
 		if [ -d "${i}"/"${ROON_SHARE}" ] ; then
 			echo "${i}/${ROON_SHARE}"
 			break 1
@@ -11,8 +11,8 @@ for i in $VOLS; do
 done
 
 #Checking for RoonServer Share on external drives
-DEVS=`/usr/syno/bin/synousbdisk -enum|grep -v "^Total"`
-for i in $DEVS; do
+EXTDRIVES=`/usr/syno/bin/synousbdisk -enum|grep -v "^Total"`
+for i in $EXTDRIVES; do
 	USB_SHARE_NAME=`/usr/syno/bin/synousbdisk -info $i | grep "Share Name" | awk '{print $3}'`
 	if [ "${USB_SHARE_NAME}" == "${ROON_SHARE}" ]; then
 		USB_MOUNT=`/usr/syno/bin/synousbdisk -info $i | grep "Mount Path" | awk '{print $3}'`
